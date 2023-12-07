14fc5215a56e2fcce415eaeccd8ba6625eec632a
Sanjana Nimesh <127377258+IT22102546@users.noreply.github.com>
2023-12-08 01:58:31 +0530

    basic completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
+import CounterClass from './components/CounterClass';
+import CounterFunction from './components/CounterFunction';
 
 
 
 function App() {
   return (
     <div className="App">
-     <h3>Hi</h3>
+     <h3>Hi Welcome to React</h3>
+     <CounterClass/>
+     <CounterFunction/>
     </div>
   );
 }
