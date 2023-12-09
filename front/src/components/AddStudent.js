54a5cbc147f46c1ed193439adb7c8c0a9015b334
Sanjana Nimesh <127377258+IT22102546@users.noreply.github.com>
2023-12-09 23:48:00 +0530

    addDetails

--- front/src/components/AddStudent.js
+++ front/src/components/AddStudent.js
@@ -1,39 +1,79 @@
-import React from "react";
+import React,{useState} from "react";
+
 
 
 export default function AddStudent()
 {
+
+  const [name,setName] = useState("");
+  const [age,setAge] = useState("");
+  const [gender,setGender] = useState("");
+  const [mobile,setMobile] = useState("");
+
+
+  function sendData(e)
+  {
+    e.preventDefault();
+    
+    const newStudent = {
+
+      name,
+      age,
+      gender,
+      mobile
+
+    }
+
+    console.log(newStudent);
+  }
+
     return(
     
       <div className="container"> 
-        <form>
+        <form onSubmit={sendData}>
 
-        <div class="mb-3">
-          <label for="name" class="form-label">Enter Student Name</label>
-          <input type="text" class="form-control" id="name" /> 
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">Enter Student Name</label>
+          <input type="text" className="form-control" id="name" placeholder="Enter name" onChange={(e)=>{
+
+            setName(e.target.value);
+
+          }}/> 
         </div>
 
-        <div class="mb-3">
-          <label for="age" class="form-label">Enter Age</label>
-          <input type="text" class="form-control" id="age"/>
+        <div className="mb-3">
+          <label htmlFor="age" className="form-label">Enter Age</label>
+          <input type="text" className="form-control" id="age" placeholder="Enter Age"  onChange={(e)=>{
+
+              setAge(e.target.value);
+
+            }}/>
         </div>
 
-        <div class="mb-3">
-          <label for="gender" class="form-label">Enter Gender</label>
-          <input type="text" class="form-control" id="gender"/>
+        <div className="mb-3">
+          <label htmlFor="gender" className="form-label">Enter Gender</label>
+          <input type="text" className="form-control" id="gender" placeholder="Enter Gender" onChange={(e)=>{
+
+              setGender(e.target.value);
+
+            }}/>
         </div>
 
-        <div class="mb-3">
-          <label for="mobile" class="form-label">Enter Mobile</label>
-          <input type="text" class="form-control" id="mobile"/>
+        <div className="mb-3">
+          <label htmlFor="mobile" className="form-label">Enter Mobile</label>
+          <input type="text" className="form-control" id="mobile" placeholder="Enter Mobile Number" onChange={(e)=>{
+
+              setMobile(e.target.value);
+
+            }}/>
         </div>
 
 
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
       </div>  
     )
